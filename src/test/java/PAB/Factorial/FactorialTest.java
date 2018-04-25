ea9a58385df95b3292d6db42c1050552bb0e02a2
Juananprimero <0619482117@uma.es>
2018-04-25 10:26:07 +0200

    He hecho muchos cambios jijijij

--- src/test/java/PAB/Factorial/FactorialTest.java
+++ src/test/java/PAB/Factorial/FactorialTest.java
@@ -2,33 +2,41 @@ package PAB.Factorial;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class FactorialTest {
+	
+	private Factorial factorial;
+	@Before
+	public void init(){
+		factorial = new Factorial();
+	}
+	
 	@Test
 	public void shouldFactorialOfZeroreturnOne(){
-		Factorial factorial1= new Factorial();
-		int result = factorial1.compute(0);
+		
+		int result = factorial.compute(0);
 		assertEquals(1, result);
 		
 	}
 	@Test
 	public void shouldFactorialOfoneReturnOne(){
-		Factorial factorial= new Factorial();
+		
 		int result = factorial.compute(1);
 		int expectedResult=1;
 		assertEquals(expectedResult, result);
 	}
 	@Test
 	public void shouldFactorialOfTwoReturntwo(){
-		Factorial factorial= new Factorial();
+
 		int result = factorial.compute(2);
 		int expectedResult=2;
 		assertEquals(expectedResult, result);
 	}
 	@Test
 	public void shouldFactorialOfThreeReturnSix(){
-		Factorial factorial= new Factorial();
+	
 		int result = factorial.compute(3);
 		int expectedResult=6;
 		assertEquals(expectedResult, result);
